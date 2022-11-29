5b474c0987d6fb4520fd076f284bed719e9a7178
itsFilipa <a.canelhas@campus.fct.unl.pt>
2022-11-29 13:20:45 +0000

    populate fake data

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const App: React.FC = () => {
   // create the database tables if they don't exist
   useEffect(() => {
     setupTables();
-    populateFakeData();
+    // populateFakeData();
     setCurrentUser();
   }, [setCurrentUser]);
 
